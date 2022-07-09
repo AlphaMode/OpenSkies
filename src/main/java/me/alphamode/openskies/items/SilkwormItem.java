1c7738164cb89b144fa54bdf8fcba8f0ba9a3aab
maximumpower55 <maximumpowermc@gmail.com>
2022-07-08 20:01:53 -0700

    git why 2

---
+++ src/main/java/me/alphamode/openskies/items/SilkwormItem.java
@@ -0,0 +1,26 @@
+package me.alphamode.openskies.items;
+
+import me.alphamode.openskies.OpenBlocks;
+import me.alphamode.openskies.blocks.InfestedLeavesBlock;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class SilkwormItem extends Item {
+    public SilkwormItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext useOnContext) {
+        BlockState state = useOnContext.getLevel().getBlockState(useOnContext.getClickedPos());
+        if (state.is(BlockTags.LEAVES) && !state.is(OpenBlocks.INFESTED_LEAVES)) {
+            InfestedLeavesBlock.infestLeafBlock(useOnContext.getLevel(), useOnContext.getClickedPos());
+            useOnContext.getItemInHand().shrink(1);
+            return InteractionResult.SUCCESS;
+        }
+        return super.useOn(useOnContext);
+    }
+}
