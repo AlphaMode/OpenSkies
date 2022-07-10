b7cd8900ee7cd6dd0ff9edf3d50a08fa8fb6cb07
alpha <alphamodeyt@gmail.com>
2022-07-09 23:34:00 -0500

    Improve Leaves type for infested leaves and add item group lang

--- src/main/java/me/alphamode/openskies/compat/probe/InfestedLeavesProvider.java
+++ src/main/java/me/alphamode/openskies/compat/probe/InfestedLeavesProvider.java
@@ -4,6 +4,7 @@ import mcjty.theoneprobe.api.*;
 import me.alphamode.openskies.OpenBlockEntities;
 import me.alphamode.openskies.OpenSkies;
 import me.alphamode.openskies.blocks.entity.InfestedLeavesBlockEntity;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
@@ -21,6 +22,7 @@ public class InfestedLeavesProvider implements IProbeInfoProvider {
     public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, Player player, Level world, BlockState blockState, IProbeHitData data) {
         Optional<InfestedLeavesBlockEntity> be = world.getBlockEntity(data.getPos(), OpenBlockEntities.INFESTED_LEAVES);
         if (be.isPresent()) {
+            probeInfo.text(CompoundText.createLabelInfo("Leaves Type: ", I18n.get(be.get().getLeavesState().getBlock().getDescriptionId())));
             if (be.get().getProgress() >= 1.0F) {
                 probeInfo.text(CompoundText.createLabelInfo("Progress: ", "Done"));
             } else {
